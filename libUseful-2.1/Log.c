92569fe667dd113876e8f24b96017de73a2a1d25
Colum Paget <colums.projects@gmail.com>
2015-12-27 14:53:49 +0000

    changes to compile on Nilsology's macbook

--- libUseful-2.1/Log.c
+++ libUseful-2.1/Log.c
@@ -184,7 +184,7 @@ int LogFileInternalWrite(TLogFile *LF, STREAM *S, int Flags, const char *Str)
 
 		if (ParentPID==0) ParentPID=getpid();
 		if (LF) S=LogFileInternalDoRotate(LF);
-		if (! S) return;
+		if (! S) return(result);
 
 		gettimeofday(&Now,NULL);
 		if (Flags & LOGFILE_TIMESTAMP)
